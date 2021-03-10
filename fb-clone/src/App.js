eebcd976f466247a84d76341eea11ea9096d3f83
sorabh pancholi <spancholi32@gmail.com>
2021-03-11 03:12:43 +0530

    side bar

--- fb-clone/src/App.js
+++ fb-clone/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/Header'
+import LeftSidebar from './components/LeftSidebar'
 
 const App = () => {
     return (
         <>
             <Header/>
+            <LeftSidebar/>
         </>
     )
 }
